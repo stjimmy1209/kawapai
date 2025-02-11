1c478dab3b82d6115234cd4e00e6d123f97ee930
Haiyouer Aiyilaiti <haiyouer@outlook.com>
2025-02-10 21:58:20 -0600

    format code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,16 @@
 package org.ghayuros
 
 import io.github.oshai.kotlinlogging.KotlinLogging
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import java.io.IOException
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
-import java.io.IOException
-
-
 
 suspend fun main(args: Array<String>) {
-        val geminiBody = "{\"contents\": [{ \"parts\": [{\"text\": \"${args[0]}\"}] }]}"
-        val localLLMBody = """{
+  val geminiBody = "{\"contents\": [{ \"parts\": [{\"text\": \"${args[0]}\"}] }]}"
+  val localLLMBody =
+      """{
     "model": "deepseek-r1-distill-qwen-14b",
     "messages": [
     { "role": "user", "content": "How to learn Kotlin?" }
@@ -23,43 +19,40 @@ suspend fun main(args: Array<String>) {
     "max_tokens": 5000,
     "stream": false
     }"""
-    val response = callLocalLLM(localLLMBody)
-    println(response)
+  val response = callLocalLLM(localLLMBody)
+  println(response)
 }
 
 fun callGemini(input: String): String {
-    val logger = KotlinLogging.logger {}
-    val apiKey = "AIzaSyB2VA1IO8Pc_GCmXh88AEgt25v6Pk02C1w"
-    val url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}"
-    val request = Request.Builder()
-        .url(url)
-        .post(input.toRequestBody("application/json".toMediaType()))
-        .build()
-    val client = OkHttpClient()
+  val logger = KotlinLogging.logger {}
+  val apiKey = "AIzaSyB2VA1IO8Pc_GCmXh88AEgt25v6Pk02C1w"
+  val url =
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}"
+  val request =
+      Request.Builder().url(url).post(input.toRequestBody("application/json".toMediaType())).build()
+  val client = OkHttpClient()
 
-    client.newCall(request).execute().use { response ->
-        if (!response.isSuccessful){
-            logger.error { response.message }
-            throw IOException("Unexpected code $response")
-        }
-        return response.body!!.string()
+  client.newCall(request).execute().use { response ->
+    if (!response.isSuccessful) {
+      logger.error { response.message }
+      throw IOException("Unexpected code $response")
     }
+    return response.body!!.string()
+  }
 }
 
 suspend fun callLocalLLM(input: String): String {
-    val logger = KotlinLogging.logger {}
-    val url = "http://localhost:1234/api/v0/chat/completions"
-    val request = Request.Builder()
-        .url(url)
-        .post(input.toRequestBody("application/json".toMediaType()))
-        .build()
-    val client = OkHttpClient()
+  val logger = KotlinLogging.logger {}
+  val url = "http://localhost:1234/api/v0/chat/completions"
+  val request =
+      Request.Builder().url(url).post(input.toRequestBody("application/json".toMediaType())).build()
+  val client = OkHttpClient()
 
-    client.newCall(request).execute().use { response ->
-        if (!response.isSuccessful){
-            logger.error { response.message }
-            throw IOException("Unexpected code $response")
-        }
-        return response.body!!.string()
+  client.newCall(request).execute().use { response ->
+    if (!response.isSuccessful) {
+      logger.error { response.message }
+      throw IOException("Unexpected code $response")
     }
+    return response.body!!.string()
+  }
 }
