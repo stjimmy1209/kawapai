9179431453635dec7d402471f22f1be20dc2388b
Haiyouer Aiyilaiti <haiyouer@outlook.com>
2025-02-10 21:21:49 -0600

    local LLM - connection established. socket closed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,15 +9,43 @@ import java.io.IOException
 
 
 fun main(args: Array<String>) {
-    val body = "{\"contents\": [{ \"parts\": [{\"text\": \"${args[0]}\"}] }]}"
-    val response = callGemini(body)
+    val geminiBody = "{\"contents\": [{ \"parts\": [{\"text\": \"${args[0]}\"}] }]}"
+    val localLLMBody = """{
+    "model": "deepseek-r1-distill-qwen-14b",
+    "messages": [
+    { "role": "system", "content": "Always answer in rhymes." },
+    { "role": "user", "content": "Introduce yourself." }
+    ],
+    "temperature": 0.7,
+    "max_tokens": 5000,
+    "stream": false
+    }"""
+    val response = callLocalLLM(localLLMBody)
     println(response)
 }
 
 fun callGemini(input: String): String {
     val logger = KotlinLogging.logger {}
-    val apiKey = "AIzaSyBKZc8z-iwiefPT6R28kGTjFj-XXFSFS58"
+    val apiKey = "AIzaSyB2VA1IO8Pc_GCmXh88AEgt25v6Pk02C1w"
     val url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}"
+    val request = Request.Builder()
+        .url(url)
+        .post(input.toRequestBody("application/json".toMediaType()))
+        .build()
+    val client = OkHttpClient()
+
+    client.newCall(request).execute().use { response ->
+        if (!response.isSuccessful){
+            logger.error { response.message }
+            throw IOException("Unexpected code $response")
+        }
+        return response.body!!.string()
+    }
+}
+
+fun callLocalLLM(input: String): String {
+    val logger = KotlinLogging.logger {}
+    val url = "http://localhost:1234/api/v0/chat/completions"
     val request = Request.Builder()
         .url(url)
         .post(input.toRequestBody("application/json".toMediaType()))
